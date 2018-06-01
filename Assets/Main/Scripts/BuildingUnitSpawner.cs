550d84e61258ca4b1ae905a67a75819ae3ac5839
jpthek9 <jpthehk9@gmail.com>
2018-05-31 19:23:22 -1000

    Desert RTS #5: - Add unit spawner script for buildings - Create Headquarters unit - Update and reorganize LS submodule

---
+++ Assets/Main/Scripts/BuildingUnitSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lockstep;
+namespace DesertRTS {
+	public class BuildingUnitSpawner : Ability {
+
+		[FixedNumber]
+		public long spawnInterval;
+
+		[DataCode ("Agents")]
+		public string spawnAgentCode;
+
+		long spawnAcc;
+		protected override void OnInitialize ()
+		{
+			
+		}
+		protected override void OnSimulate ()
+		{
+			spawnAcc += LockstepManager.DeltaTime;
+			if (spawnAcc >= spawnInterval) {
+				spawnAcc = 0;
+				SpawnUnit ();
+			}
+		}
+		void SpawnUnit () {
+			Agent.Controller.CreateAgent (spawnAgentCode, Agent.Body.Position);
+		}
+	}
+}
