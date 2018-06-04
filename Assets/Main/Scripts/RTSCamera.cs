5bf10a3840b50154d5100ca63e0162684070b0e6
jpthek9 <jpthehk9@gmail.com>
2018-06-03 20:29:57 -1000

    Desert RTS #6 - RTS Camera: - Add RTS Camera - Scene integration

---
+++ Assets/Main/Scripts/RTSCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSCamera : MonoBehaviour {
+	public float MoveSpeed = 8f;
+	public float ZoomSpeed = 4f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horInput = Input.GetAxis ("Horizontal");
+		float verInput = Input.GetAxis ("Vertical");
+
+		float zoomInput = Input.GetAxis ("Mouse ScrollWheel");
+
+		Vector3 move = new Vector3 (horInput, 0f, verInput);
+		move *= Time.deltaTime;
+		move *= MoveSpeed;
+		transform.position += move;
+
+		Vector3 zoom = new Vector3 (0, 0, zoomInput);
+		zoom *= ZoomSpeed;
+		transform.Translate (zoom, Space.Self);
+	}
+}
